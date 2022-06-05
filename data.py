b2af720b8a6237fe3ceb02c5a2844dd9d749cc35
Ville Rytinki <66476143+VilleRytinki@users.noreply.github.com>
2022-06-05 11:51:44 +0300

    Created functions for getting the item data and transforming the data to include only the stuff that are relevant to the application.

--- data.py
+++ data.py
@@ -4,6 +4,7 @@ import requests
 import numpy as np
 
 
+
 @contextlib.contextmanager
 def request_http(address_str):
     """
@@ -89,6 +90,66 @@ def load_list_to_csv(file_name, id_list):
     print("A total of {} id's were written to {}.csv".format(len(data_list_np), file_name))
 
 
+def get_item_data(item_id):
+    """
+
+    Parameters
+    ----------
+    item_id: int
+        The id of the item you want attributes for.
+
+    Returns
+    -------
+    dict:
+        Returns a dictionary of the attributes for the given item.
+
+    """
+
+    with request_http('https://esi.evetech.net/latest/universe/types/{}/?datasource=tranquility&language=en'.format(
+            item_id)) as request:
+        return request.json()
+
+
+def transform_item_data(item_data):
+    """
+    This function transforms the item data to a new dictionary with the data relevant for the app.
+
+    Parameters
+    ----------
+    item_data: dict
+        Dictionary of the data.
+
+    Returns
+    -------
+    dict:
+        Returns a dictionary with the transformed format.
+
+    Notes
+    -------
+        The format of the returned data is as follows:
+        index | market_group_id | name | volume | packaged_volume
+
+    """
+    new_dict = {'market_group_id': 0, 'name': '', 'volume': 0,
+                'packaged_volume': 0}  # create a template for a new dict.
+
+    '''insert the data value from the request to the appropriate key'''
+    new_dict['market_group_id'] = item_data['market_group_id']
+    new_dict['name'] = item_data['name']
+    new_dict['volume'] = item_data['volume']
+    new_dict['packaged_volume'] = item_data['packaged_volume']
+
+    return new_dict
+
+
 '''test code'''
-id_list = get_all_items_id()
-load_list_to_csv()('item_id', id_list)
+# id_list = get_all_items_id()
+# load_list_to_csv()('item_id', id_list)
+
+id_list = np.loadtxt("item_id.csv", delimiter=',', dtype=int)
+item_id = id_list[65]
+print(item_id)
+item_data = get_item_data(item_id)
+transformed_data = transform_item_data(item_data)
+
+
